fbdb514cd9bf1e2e6826d739cf5d10321d9e057a
Johana Castellano <johanacg16@gmail.com>
2025-02-04 14:19:03 +0000

    html elements in app

--- discord-hero-app/src/App.jsx
+++ discord-hero-app/src/App.jsx
@@ -5,6 +5,18 @@ export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <main>
+        <h1>IMAGINE A PLACE...</h1>
+        <p>...where you can belong to a school club, a gaming group, or a <br />
+        worldwide art community. Where just you and a handful of <br />
+        friends can spend time together. A place that makes it easy to <br />
+        talk every day and hang out more often.</p>
+        <button>Download for Mac</button>
+        <button>Open Discord in your browser</button>
+      </main>
+      <aside>
+        <img src="./src/assets/discord-background.png" alt="discord-background" /> 
+      </aside>
     </div>
   )
 }
