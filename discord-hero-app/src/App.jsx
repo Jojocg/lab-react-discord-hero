35a3b9c080bbd71fd91930d1d270e1b23d088469
Johana Castellano <johanacg16@gmail.com>
2025-02-04 12:47:38 +0000

    navbar component

--- discord-hero-app/src/App.jsx
+++ discord-hero-app/src/App.jsx
@@ -1,9 +1,11 @@
-
 import './App.css'
+import Navbar from './components/navbar'
 
 export default function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Navbar />
+    </div>
   )
 }
 
---
+++ discord-hero-app/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+/* import discordLogo from '../src/assets/discord-logo-white.png'; */
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../src/assets/discord-logo-white.png" alt="discord-logo" />
+      <img src="../src/assets/menu-icon.png" alt="menu-icon" />
+    </nav>
+  );
+}
