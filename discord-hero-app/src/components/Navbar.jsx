5383f02d656abe86f5245cb52f2a75e5d0d8fe1d
Johana Castellano <johanacg16@gmail.com>
2025-02-04 12:57:00 +0000

    background-color and first changes on css

--- discord-hero-app/src/components/Navbar.jsx
+++ discord-hero-app/src/components/Navbar.jsx
@@ -3,8 +3,8 @@
 export default function Navbar() {
   return (
     <nav>
-      <img src="../src/assets/discord-logo-white.png" alt="discord-logo" />
-      <img src="../src/assets/menu-icon.png" alt="menu-icon" />
+      <img className="discord-logo" src="../src/assets/discord-logo-white.png" alt="discord-logo" />
+      <img className="menu-icon" src="../src/assets/menu-icon.png" alt="menu-icon" />
     </nav>
   );
 }
