b785152a9bf3e69286861b3eedfb7ca5ce5f19d5
Johana Castellano <johanacg16@gmail.com>
2025-02-04 14:55:08 +0000

    done

--- discord-hero-app/src/App.jsx
+++ discord-hero-app/src/App.jsx
@@ -6,13 +6,13 @@ export default function App() {
     <div className="App">
       <Navbar />
       <main>
-        <h1>IMAGINE A PLACE...</h1>
-        <p>...where you can belong to a school club, a gaming group, or a <br />
+        <h1 className='heading'>IMAGINE A <br />PLACE...</h1>
+        <p className='main-text'>...where you can belong to a school club, a gaming group, or a <br />
         worldwide art community. Where just you and a handful of <br />
         friends can spend time together. A place that makes it easy to <br />
         talk every day and hang out more often.</p>
-        <button>Download for Mac</button>
-        <button>Open Discord in your browser</button>
+        <button className='download-btn'>Download for Mac</button>
+        <button className='browser-btn'>Open Discord in your browser</button>
       </main>
       <aside>
         <img src="./src/assets/discord-background.png" alt="discord-background" /> 
