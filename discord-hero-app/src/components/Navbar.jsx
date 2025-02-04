3f6d9c768a7a6b0036060bd8039c6aba559642cc
Johana Castellano <johanacg16@gmail.com>
2025-02-04 13:09:51 +0000

    structured navbar

--- discord-hero-app/src/components/Navbar.jsx
+++ discord-hero-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="discord-navbar">
       <img className="discord-logo" src="../src/assets/discord-logo-white.png" alt="discord-logo" />
       <img className="menu-icon" src="../src/assets/menu-icon.png" alt="menu-icon" />
     </nav>
